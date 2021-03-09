702b66307e7e5ff659a382d9d4e618225c80d2dd
Hermosilla Reinaldo <ghost_cad@hotmail.com>
2021-03-09 01:37:20 -0300

    modified poison method

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -16,11 +16,8 @@ namespace BepInExPlugin
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
         }
 
-        [HarmonyPatch(typeof(SE_Burning), "Setup")]
         public static class DelayFire
         {
-            private static float m_totalDamage2;
-
             
             private static void Postfix(SE_Burning __instance)
             {
@@ -29,17 +26,19 @@ namespace BepInExPlugin
 
             [HarmonyPrefix]
             [HarmonyPatch(typeof(SE_Burning),"AddFireDamage")]
-            static bool Prefix(ref float damage, ref float ___m_totalDamage, ref float ___m_damageInterval, 
+            static bool AddFireDamage(ref float damage, ref float ___m_totalDamage, ref float ___m_damageInterval, 
                 ref float ___m_ttl, ref HitData.DamageTypes ___m_damage, SE_Burning __instance)
             {
-                //private HitData.DamageTypes m_damage;
                 ___m_totalDamage = Mathf.Max(___m_totalDamage, damage);
                 int num = (int)(___m_ttl / ___m_damageInterval);
                 float fire = ___m_totalDamage / (float)num;
-                ___m_damage.m_fire = fire;
+                ___m_damage.m_fire = fire * 0.5f;
                 __instance.ResetTime();
                 return false;
             }
+
+            
+
         }
        
         [HarmonyPatch(typeof(Console), "InputText")]
@@ -51,6 +50,28 @@ namespace BepInExPlugin
             {
                 string newtext = __instance.m_input.text;
                 float.TryParse(newtext, out lol);
+            }
+        }
+
+        public static class DelayPoison
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(SE_Poison), "AddDamage")]
+            static bool AddDamage(SE_Poison __instance, ref float damage)
+            {
+                Debug.Log("damageeeee: " + damage);
+
+                if (damage >= __instance.m_damageLeft)
+                {
+                    __instance.m_damageLeft = damage;
+                    float num = (__instance.m_character.IsPlayer() ? __instance.m_TTLPerDamagePlayer : __instance.m_TTLPerDamage);
+                    __instance.m_ttl = __instance.m_baseTTL + Mathf.Pow(__instance.m_damageLeft * num, __instance.m_TTLPower);
+                    int num2 = (int)(__instance.m_ttl / __instance.m_damageInterval);
+                    __instance.m_damagePerHit = __instance.m_damageLeft / (float)num2;
+                    __instance.m_damagePerHit = __instance.m_damagePerHit * 0.5f;
+                    __instance.ResetTime();
+                }
+                return false;
             }
         }
     }
